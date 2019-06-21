bdc46c212a5f90d94108bf1dff6b43fb3663861d
smukherjee16 <tosayari@yahoo.com>
2019-06-21 14:40:24 -0400

    initial project commit

---
+++ src/main/java/com/sayari/sayaripetclinic/SayariPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sayari.sayaripetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayariPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayariPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sayari/sayaripetclinic/SayariPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sayari.sayaripetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SayariPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
